b1c5a7c68033038319b3e95ea2ed5f08bf16f630
baliraptorr <baliraptorr02@gmail.com>
2022-07-31 15:41:37 +0200

    testy

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,20 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+--space as leader
+keymap("n", "<Space>", "", opts)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--NvimTreeToggle
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+
+--GoRun
+keymap("n", "<leader>r", ":GoRun<cr>", opts)
+
+--save
+keymap("n", "<C-s>", "<Esc>:w<cr>", opts)
+
+--split
+keymap("n", "<C-v>", ":vsplit<cr>", opts)
